c3a15e7cead3d04db5ce907e913d59c57005669e
NabilaRahmaAulia <d1041231030@student.untan.ac.id>
2026-02-05 15:42:13 +0700

    Nabila Rahma Aulia, D1041231030, 05-02-2026

---
+++ src/KodePertama_Latihan1.java
@@ -0,0 +1,8 @@
+public class KodePertama_Latihan1 {
+    public static void main(String [] args){
+        System.out.println("Nama: Nabila Rahma Aulia");
+        System.out.println("NIM: D1041231030");
+        System.out.println("Semester: 6");
+        System.out.println("Prodi: Informatika");
+    }
+}
