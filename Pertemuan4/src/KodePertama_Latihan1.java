cfb49dc8572cf973e9fb31c1d37104b047f64c6e
NabilaRahmaAulia <d1041231030@student.untan.ac.id>
2026-02-19 11:40:39 +0700

    Delete Pertemuan4 directory

--- Pertemuan4/src/KodePertama_Latihan1.java
+++
@@ -1,8 +0,0 @@
-public class KodePertama_Latihan1 {
-    public static void main(String [] args){
-        System.out.println("Nama: Nabila Rahma Aulia");
-        System.out.println("NIM: D1041231030");
-        System.out.println("Semester: 6");
-        System.out.println("Prodi: Informatika");
-    }
-}
